df3e86c0f0d258c3bc7e6584eb83173f66d69b56
serraguti <pacoserranox@gmail.com>
2024-05-21 11:48:03 +0200

    Add project files.

---
+++ MvcCoreSeriesBeanstalk/Data/SeriesContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreSeriesBeanstalk.Models;
+
+namespace MvcCoreSeriesBeanstalk.Data
+{
+    public class SeriesContext: DbContext
+    {
+        public SeriesContext(DbContextOptions<SeriesContext> options)
+            : base(options) { }
+
+        public DbSet<Serie> Series { get; set; }
+    }
+}
---
+++ MvcCoreSeriesBeanstalk/Repositories/RepositorySeries.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreSeriesBeanstalk.Data;
+using MvcCoreSeriesBeanstalk.Models;
+
+namespace MvcCoreSeriesBeanstalk.Repositories
+{
+    public class RepositorySeries
+    {
+        private SeriesContext context;
+
+        public RepositorySeries(SeriesContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Serie>> GetSeriesAsync()
+        {
+            return await this.context.Series.ToListAsync();
+        }
+
+        private async Task<int> GetMaxIdSerie()
+        {
+            return await this.context.Series.MaxAsync
+                (x => x.IdSerie) + 1;
+        }
+
+        public async Task CreateSerieAsync
+            (string nombre, string imagen, int anyo)
+        {
+            Serie serie = new Serie
+            {
+                IdSerie = await this.GetMaxIdSerie(),
+                Nombre = nombre, 
+                Imagen = imagen, 
+                Anyo = anyo
+            };
+            this.context.Series.Add(serie);
+            await this.context.SaveChangesAsync();
+        }
+    }
+}
